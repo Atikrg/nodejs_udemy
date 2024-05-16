1d22a5d5ee1395c879713ce517673b2b2079bc62
Atikrg <email>
2024-05-17 02:30:56 +0530

    create a first api

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 const express = require('express')
+const fs = require('fs');
 
 const app = express();
 
-app.get('/',(req, res)=>{
+/* app.get('/',(req, res)=>{
   res.status(200)
   .json({
     message: "hello from the server",
     app: "Natours"
   })
 })
+ */
+
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: {
+      tours
+    }
+  })
+})
 
 const port = 3000;
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log(`App is running on ${port}`)
 })
