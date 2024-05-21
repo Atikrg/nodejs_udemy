dbbf28e470ca192437762a4ff712037e7a80b8b9
Atikrg <email>
2024-05-21 19:01:57 +0530

    66: SERVING STATIC FILES

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const userRouter = require('./routes/userRoutes');
 //1. Middlewares
 app.use(morgan('dev')); //morgan thirdparty middleware
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`))
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware 👍👍👍👍');
