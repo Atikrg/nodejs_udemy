a37fa9783c8cbf914ce2a17ed86fcfe847ec4ca6
Atikrg <email>
2024-05-23 12:59:50 +0530

    94. Importing Development Data

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const fs = require('fs');
+const Tour = require('./../../models/tourModel')
+dotenv.config({path: './config.env'}); 
+
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+
+mongoose.connect(DB,{
+  useNewUrlParser: true,
+  useCreateIndex:true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+}).then(con =>{
+ /*  console.log(con.connections); */
+  console.log('DB connection successfull!');
+});
+
+// READ JSON FILE
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+
+//IMPORT DATA INTO DATABASE
+const importData = async () => {
+    try{
+        await Tour.create(tours);
+        console.log("Data successfully loaded !");
+        process.exit();
+    }catch(error){
+        console.log(error);
+    }
+}
+// DELETE ALL DATA FROM COLLECTION
+const deleteData = async()=>{
+    try{
+        await Tour.deleteMany();
+        console.log("Data Successfully deleted!");
+        console.log(`${__dirname}`)
+        process.exit()
+        
+    }catch(error)
+    {
+        console.log(error);
+    }
+}
+
+if(process.argv[2] == '--import'){
+    importData()
+}else if(process.argv[2] == '--delete'){
+    deleteData();
+}
+// shows the array of path
+console.log(process.argv);
