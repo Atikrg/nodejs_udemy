4ef4e5b4171e084b0c7dc180bd929bea9ff7f3e3
Atikrg <email>
2024-05-25 00:39:16 +0530

    100. Making the API Better:Aliasing

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -14,7 +14,7 @@ exports.getAllTours = async (req, res) => {
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
-    
+
     //1B. Advanced Filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
@@ -23,15 +23,39 @@ exports.getAllTours = async (req, res) => {
     let query = Tour.find(JSON.parse(queryStr));
 
     // 2) SORTING
-    if(req.query.sort){
+    if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
       console.log(sortBy);
       query = query.sort(sortBy);
-      
-    }else{
-      query = query.sort('-createdAt')
+    } else {
+      query = query.sort('-createdAt');
     }
-    
+
+    // 3. Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v'); // - excluding
+    }
+
+    //4 Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    //page=2&limit=10, 1-10 -> page 1, 11-20 -> page2
+    query = query.skip(skip).limit(limit);
+    /*   query = query.skip(2).limit(10); */
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments(); //returns a promise
+
+      if (skip >= numTours) {
+        throw new Error(`This Page does not exist`);
+      }
+    }
+
     //EXECUTE QUERY
     const tours = await query;
 
